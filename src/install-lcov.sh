9156a2e75830c70d20307421d99a9a377ac9ad6c
Hrishikesh Kadam <hrkadam.92@gmail.com>
2024-08-02 01:13:09 +0530

    Check on mac

--- src/install-lcov.sh
+++ src/install-lcov.sh
@@ -1,32 +1,28 @@
 #!/usr/bin/env bash
 
 # Arguments:
-#   $1 INSTALL_HEAD_VERSION true / false
+#   $1 REF Git reference like branch or tag of linux-test-project/lcov repository. 
+#      Use HEAD for the default branch.
+#      Empty for stable versions.
 
 set -e -o pipefail
 
+REF=$1
+
 if [[ $(uname -s) =~ ^"Linux" ]]; then
-  if [[ $INSTALL_HEAD_VERSION != "true" ]]; then
+  if [[ $REF == "" ]]; then
     sudo apt-get -y install lcov
   else
-    # https://github.com/linux-test-project/lcov/blob/d465f73117ac3b66e9f6d172346ae18fcfaf0f69/README#L116-L162
-    # https://www.cpan.org/modules/INSTALL.html
-    sudo cpan App::cpanminus
-    sudo cpanm \
-      --notest \
-      Capture::Tiny \
-      DateTime
-    LCOV_DIR="/tmp/lcov"
-    git clone \
-      --depth 1 \
-      https://github.com/linux-test-project/lcov.git \
-      $LCOV_DIR
-    pushd $LCOV_DIR &> /dev/null
-    sudo make install
-    popd &> /dev/null
+    make_install "$REF"
   fi
 elif [[ $(uname -s) =~ ^"Darwin" ]]; then
-  brew install lcov
+  if [[ $REF == "" ]]; then
+    brew install lcov
+  elif [[ $REF == "HEAD" ]]; then
+    brew install --HEAD lcov
+  else
+    make_install "$REF"
+  fi
 elif [[ $(uname -s) =~ ^"MINGW" ]]; then
   choco install lcov
   if [[ $GITHUB_ACTIONS == "true" ]]; then
@@ -42,3 +38,32 @@ else
   echo "Unknown OS: $(uname -s)"
   exit 1
 fi
+
+#######################################
+# Arguments:
+#   $1 REF Git reference like branch or tag of linux-test-project/lcov repository. 
+#      Use HEAD for the default branch.
+#######################################
+make_install() {
+  REF=$1
+  # https://github.com/linux-test-project/lcov/blob/d465f73117ac3b66e9f6d172346ae18fcfaf0f69/README#L116-L162
+  # https://www.cpan.org/modules/INSTALL.html
+  sudo cpan App::cpanminus
+  sudo cpanm \
+    --notest \
+    Capture::Tiny \
+    DateTime
+  LCOV_DIR="/tmp/lcov"
+  BRANCH_ARG=""
+  if [[ $REF != "HEAD" ]]; then
+    BRANCH_ARG="--branch $REF"
+  fi
+  git clone \
+    "$BRANCH_ARG" \
+    --depth 1 \
+    https://github.com/linux-test-project/lcov.git \
+    $LCOV_DIR
+  pushd $LCOV_DIR &> /dev/null
+  sudo make install
+  popd &> /dev/null
+}
